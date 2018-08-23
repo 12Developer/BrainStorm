5995700a77781a9860e9fd8cca726dc264241cc7
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2018-08-23 13:54:36 -0700

    fixed typo in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User
   if(ENV['DATABASE_URL'])
-      uri = URI.parse(ENV['DATABASE_ URL'])
+      uri = URI.parse(ENV['DATABASE_URL'])
       DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   else
       DB = PG.connect(:host => "localhost", :port => 5432, :dbname => 'ideaApp_development')
